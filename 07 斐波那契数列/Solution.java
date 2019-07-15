c86f53f4b3f82c44fe5caffa04045006b9ad48e1
SeluneXscd <1353977352@qq.com>
2019-07-15 19:10:53 +0800

    add 07 斐波那契数列

---
+++ 07 斐波那契数列/Solution.java
@@ -0,0 +1,22 @@
+public class Solution{
+	public int Fibonacci(int n) {
+		if (n < 0 || n > 39) {
+			return -1;
+		}
+		if (n <= 1) {
+			return n;
+		}
+		int[] fib = new int[n + 1];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		return fib[n];
+	}
+
+	public static void main(String[] args) {
+		offer07 offer = new offer07();
+		System.out.println(offer.Fibonacci(0));
+	}
+}
